31b59009d94594ce43493135db3d356548a3451f
mamipokok <nabilhasyaartuna@gmail.com>
2024-09-26 10:42:38 +0700

    page - page dalam sidebar

---
+++ lib/pages/sidebar/page_sidebar/metode_bayar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MetodeBayar extends StatelessWidget {
+  const MetodeBayar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Metode Bayar"),
+    );
+  }
+}
---
+++ lib/pages/sidebar/page_sidebar/printer_struck.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PrinterStruck extends StatelessWidget {
+  const PrinterStruck({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Printer & struck"),
+    );
+  }
+}
