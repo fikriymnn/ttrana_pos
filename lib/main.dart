3a43c6c682bd86489916d68484418b00e4eb6b6a
iqbaaleff <iqbaalfadillah19@gmail.com>
2024-11-15 11:28:15 +0700

    pull dari nabil

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Aplikasi POS',
-      home: MainPageKasir(),
+      home: MainPage(),
     );
   }
 }
